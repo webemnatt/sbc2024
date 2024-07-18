58547cc9250d1b424b9494b7830a89d566ef6b09
webemnatt <webemnatt@gmail.com>
2024-07-18 09:22:47 -0300

    first commit.

---
+++ src/test/java/project/my/sbc2024/Sbc2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package project.my.sbc2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sbc2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
