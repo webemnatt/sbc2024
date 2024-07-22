8f201fedde57521ab8578d0e25bc0e91050f1f51
webemnatt <webemnatt@gmail.com>
2024-07-22 10:51:36 -0300

    bugFix: CORS.

--- src/main/java/project/my/sbc2024/Application.java
+++ src/main/java/project/my/sbc2024/Application.java
@@ -3,6 +3,10 @@ package project.my.sbc2024;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(servers = {@Server(url = "/", description="Default server URL")})
 @SpringBootApplication
 public class Application {
 
